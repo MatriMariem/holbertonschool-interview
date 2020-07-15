013c338b2f67faf91226ba66ceb8ebd4ccaa868a
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-07-15 10:57:20 +0100

    fix pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-We have n number of locked boxes in front of us. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+We have n number of locked boxes in front of us.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
 """
+
 
 def canUnlockAll(boxes):
     """a method that determines if all the boxes can be opened"""
     Unlocked = [False for i in boxes]
     Unlocked[0] = True
     Open = [False for i in boxes]
-    while [True for i in range(len(boxes)) if Open[i] == False and Unlocked[i] == True]:
+    while [True for i in range(len(boxes))
+           if Open[i] is False and Unlocked[i] is True]:
         for k in range(len(boxes)):
-            if Unlocked[k] == True:
+            if Unlocked[k] is True:
                 Open[k] = True
                 for i in boxes[k]:
                     Unlocked[i] = True
