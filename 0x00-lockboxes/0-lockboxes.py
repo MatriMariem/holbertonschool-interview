b571de01bf965c11e92f96cdf2ff27f87e02347a
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-07-18 13:36:53 +0100

    handle 2 edge cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,8 +8,8 @@ and each box may contain keys to the other boxes.
 
 def canUnlockAll(boxes):
     """a method that determines if all the boxes can be opened"""
-    if boxes is None or type(boxes) != list or boxes == []:
-        return
+    if boxes is None or boxes == []:
+        return False
     Unlocked = [False for i in boxes]
     Unlocked[0] = True
     Open = [False for i in boxes]
