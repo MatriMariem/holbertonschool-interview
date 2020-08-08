ba732f54b46ccde075036ffddacbf9ec714797af
MatriMariem <53017898+MatriMariem@users.noreply.github.com>
2020-08-08 23:43:31 +0200

    delete is_prime function

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,23 +7,12 @@ Given a number n,
 write a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
 """
-import math
-
-
-def isprime(n):
-    """ checks if a number is prime
-    """
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
 
 
 def factorization(n):
-    """ returns the sum of prime factors of n """
-    i = 0
+    """ returns the sum of prime factors of n or n if n is prime"""
     sum = 0
-    for i in range(2, int(math.sqrt(n)) + 1):
+    for i in range(2, n + 1):
         while n % i == 0:
             n = n // i
             sum += i
@@ -36,6 +25,4 @@ def minOperations(n):
     """
     if not isinstance(n, int) or n < 2:
         return 0
-    if isprime(n):
-        return n
     return factorization(n)
