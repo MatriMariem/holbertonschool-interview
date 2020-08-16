ce69736551fec644187a705917c3510c58151de4
MatriMariem <meriemmatri1994@gmail.com>
2020-08-16 18:09:08 +0100

    sum of 2 grids without toppling

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,6 +1,28 @@
 #include "sandpiles.h"
 
 /**
+ *
+ *
+ *
+ */
+void print_grid(int grid[3][3])
+{
+    int i, j;
+
+	printf("=\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
  * sandpiles_sum - a function that computes the sum of two sandpiles
  * grid1: first grid
  * grid2: second grid
@@ -8,18 +30,23 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
+	int i, j, stable = 1;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (j)
-				printf(" ");
 			grid1[i][j] += grid2[i][j];
-			printf("%d", grid1[i][j]);
+			if (grid1[i][j] > 3)
+				stable = 0;
 		}
-		printf("\n");
+	}
+	if (stable == 1)
+		return;
+	else
+	{
+		stable = 1;
+		print_grid(grid1);
 	}
 
 }
