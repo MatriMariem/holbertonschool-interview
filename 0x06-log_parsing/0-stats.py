187c8975bba498ebf0d5ba1f44fbba49b5bbd0b7
MatriMariem <meriemmatri1994@gmail.com>
2020-08-31 20:36:48 +0100

    fix output

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,10 +5,11 @@ import sys
 
 i = 0
 FileSize = 0
-status = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
 codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-for line in sys.stdin:
-    try:
+try:
+    for line in sys.stdin:
         i += 1
         sp = line.split(' ')
         FileSize += int(sp[-1])
@@ -18,9 +19,10 @@ for line in sys.stdin:
             for code in codes:
                 if status[code]:
                     print("{}: {}".format(code, status[code]))
-    except KeyboardInterrupt:
-        print("File size: {}".format(FileSize))
-        for code in codes:
-            if status[code]:
-                print("{}: {}".format(code, status[code]))
-
+except KeyboardInterrupt:
+    print("File size: {}".format(FileSize))
+    for code in codes:
+        if status[code]:
+            print("{}: {}".format(code, status[code]))
+    raise
+    sys.exit()
