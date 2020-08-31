c51ab2e4008c83bd85850ee0adaa12d00476f2b7
MatriMariem <meriemmatri1994@gmail.com>
2020-08-31 20:18:43 +0100

    first commit

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+
+i = 0
+FileSize = 0
+status = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+for line in sys.stdin:
+    try:
+        i += 1
+        sp = line.split(' ')
+        FileSize += int(sp[-1])
+        status[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for code in codes:
+                if status[code]:
+                    print("{}: {}".format(code, status[code]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(FileSize))
+        for code in codes:
+            if status[code]:
+                print("{}: {}".format(code, status[code]))
+
