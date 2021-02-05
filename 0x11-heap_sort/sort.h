ccec65c2fdcb39c5b43b6d785950fd4676d941be
MatriMariem <meriemmatri1994@gmail.com>
2021-02-05 17:16:19 +0100

    debug Ines code

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -1,8 +1,6 @@
-#ifndef SORT_H
-#define SORT_H
-#include <stdio.h>
-#include <stdlib.h>
-void print_array(const int *array, size_t size);
+
+#ifndef _SORT_H
+#define _SORT_H
 void heap_sort(int *array, size_t size);
-void heapify(int *array, int size, int i, int last);
+void print_array(const int *array, size_t size);
 #endif
