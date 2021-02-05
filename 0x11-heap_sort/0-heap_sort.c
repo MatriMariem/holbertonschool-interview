9d6638a90f55ed48b2c473f5e7f91c3425d9979c
MatriMariem <meriemmatri1994@gmail.com>
2021-02-05 17:17:57 +0100

    debug Ines code

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -35,8 +35,8 @@ void heapify(int *array, int n, int i, size_t size)
 	if (largest != i)
 	{
 		swap(&array[i], &array[largest]);
-		heapify(array, n, largest, size);
 		print_array(array, size);
+		heapify(array, n, largest, size);
 	}
 }
 
@@ -50,7 +50,7 @@ void heap_sort(int *array, size_t size)
 {
 	int i;
 
-	if (!array || size < 1)
+	if (!array || size <= 1)
 		return;
 
 	for (i = (int)size / 2 - 1; i >= 0; i--)
