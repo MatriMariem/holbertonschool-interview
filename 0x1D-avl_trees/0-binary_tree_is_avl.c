61f1fba524684aecdb0ca78c6d3c378e40f4830f
MatriMariem <meriemmatri1994@gmail.com>
2021-03-24 00:12:49 +0100

    AVL tree

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+int height(binary_tree_t *tree)
+{
+  int hl, hr;
+
+  if (!tree)
+  return (-1);
+  hl = height(tree->left);
+  hr = height(tree->right);
+  return (hl + 1 ? hl > hr : hr + 1);
+}
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+  if (!tree)
+  return (0);
+  if (tree->left && (tree->left)->n > tree->n)
+  return (0);
+  if (tree->right && (tree->right)->n < tree->n)
+  return (0);
+  if (height(tree->left) - height(tree->right) > 1 || height(tree->left) - height(tree->right) < -1)
+  return (0);
+  if (!tree->left && !tree->right)
+  return (1);
+  if (!tree->left)
+  return (1 * binary_tree_is_avl(tree->right));
+  if (!tree->right)
+  return (1 * binary_tree_is_avl(tree->left));
+  return (1 * binary_tree_is_avl(tree->left) * binary_tree_is_avl(tree->right));
+}
---
+++ 0x1D-avl_trees/README.md
@@ -0,0 +1,6 @@
+A function that checks if a binary tree is a valid AVL Tree.
+
+# Properties of an AVL Tree:
+ - An AVL Tree is a BST
+ - The difference between heights of left and right subtrees cannot be more than one
+ - The left and right subtree each must also be a binary search tree
