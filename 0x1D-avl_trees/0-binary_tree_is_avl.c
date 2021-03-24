57fbbecf78c7deb41ac55f567e707e089468d367
MatriMariem <meriemmatri1994@gmail.com>
2021-03-24 01:13:49 +0100

    Betty coding style

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -1,13 +1,18 @@
 #include "binary_trees.h"
+/**
+ * height - Measures the height of a binary tree
+ * @tree: Pointer to the node to measures the height
+ * Return: The height of the tree starting at node @tree
+ */
 int height(binary_tree_t *tree)
 {
-  int hl, hr;
+	int hl, hr;
 
-  if (!tree)
-  return (-1);
-  hl = height(tree->left);
-  hr = height(tree->right);
-  return (hl > hr ? hl + 1 : hr + 1);
+	if (!tree)
+		return (-1);
+	hl = height(tree->left);
+	hr = height(tree->right);
+	return (hl > hr ? hl + 1 : hr + 1);
 }
 /**
  * compare_all_left_children -  compares the values of tree node
@@ -43,26 +48,34 @@ int compare_all_right_children(const binary_tree_t *tree, binary_tree_t *child)
 	return (1 * compare_all_right_children(tree, child->left)
 			* compare_all_right_children(tree, child->right));
 }
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL tree
+ * @tree: Pointer to the starting node
+ * Return: 1 if valid, and 0 otherwise
+ */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-  int cmp_left, cmp_right;
-  if (!tree)
-  return (0);
-if (!tree->left && !tree->right)
-  return (1);
+	int cmp_left, cmp_right;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
 
-cmp_left = compare_all_left_children(tree, tree->left);
-cmp_right = compare_all_right_children(tree, tree->right);
-if (!cmp_left || !cmp_right)
-  return (0);
+	cmp_left = compare_all_left_children(tree, tree->left);
+	cmp_right = compare_all_right_children(tree, tree->right);
+	if (!cmp_left || !cmp_right)
+		return (0);
 
-if (height(tree->left) - height(tree->right) > 1 || height(tree->left) - height(tree->right) < -1)
-  return (0);
+	if (height(tree->left) - height(tree->right) > 1
+			|| height(tree->left) - height(tree->right) < -1)
+		return (0);
 
-if (!tree->left)
-  return (1 * binary_tree_is_avl(tree->right));
-if (!tree->right)
-  return (1 * binary_tree_is_avl(tree->left));
-return (1 * binary_tree_is_avl(tree->left) * binary_tree_is_avl(tree->right));
+	if (!tree->left)
+		return (1 * binary_tree_is_avl(tree->right));
+	if (!tree->right)
+		return (1 * binary_tree_is_avl(tree->left));
+	return (1 * binary_tree_is_avl(tree->left) * binary_tree_is_avl(tree->right));
 }
