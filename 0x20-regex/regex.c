317aa31cc5dfa85632629cdfc029b2a82cb0cd25
MatriMariem <meriemmatri1994@gmail.com>
2021-04-13 20:36:30 +0100

    handle edge cases

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -12,9 +12,11 @@ int regex_match(char const *str, char const *pattern)
 	if (!str[0] && !pattern[0])
 		return (1);
 
-	if (str[0] == pattern[0] || (pattern[0] == '.' && pattern[1] != '*'))
+	if (str[0] == pattern[0] ||
+			(str[0] && (pattern[0] == '.' && pattern[1] != '*')))
 		return (regex_match(str + 1, pattern + 1));
-	if (str[0] != pattern[0] && (pattern[0] != '.' && pattern[1] == '*'))
+	if ((str[0] && str[0] != pattern[0])
+			&& (pattern[0] != '.' && pattern[1] == '*'))
 		return (regex_match(str, pattern + 2));
 	if (pattern[0] == '.' && pattern[1] == '*')
 	{
